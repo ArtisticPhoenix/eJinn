7102063ef5113f44400c1063039ef047778569f0
Artistic Phoenix <artisticphoenix@gmail.com>
2017-11-25 20:47:59 -0500

    namespaced

--- src/eJinn/eJinnGenerator.php
+++ src/eJinn/eJinnGenerator.php
@@ -1,6 +1,21 @@
 <?php
+namespace eJinn;
 
+use eJinn\Exception\UnknownError;
 
 class eJinnGenerator
 {
+    /**
+     * 
+     * @param mixed $conf - either an array config or a json config
+     */
+    public function __construct( $conf ){
+        if(!is_array($conf)){
+            $conf = json_decode($json, true);
+            if($code = json_last_error()){
+                throw new UnknownError(json_last_error_msg(), $code);
+            }
+        }
+    }
+    
 }
